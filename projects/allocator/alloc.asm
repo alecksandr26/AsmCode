698b1ce2e93d7f8e6cb83fc904c90e1e37b4d4f3
alecksandr <sansepiol26@gmail.com>
2022-05-24 21:15:31 -0500

    starting with deal with errors

--- projects/allocator/alloc.asm
+++ projects/allocator/alloc.asm
@@ -3,17 +3,26 @@
     section .data
     ;; Constants
     SYS_brk equ 12
+    SYS_write equ 1
+    STDOUT equ 1
 
     ;; Capacity of the heap
     CAPAICTY equ 640000         ; 64 kilo bytes
 
     NULL equ 0
+    LF equ 0xa
+
+    ;; Variables
+    size dd 0
+    
 
     section .bss
     curr_brk resq 1             ; address of the current heap
     new_brk resq 1              ; address of the new heap
     top_brk resq 1              ; address of the top of the heap
     init_brk resq 1             ; the initial address of the heap
+
+    
 
     section .text
     global alloc
@@ -23,8 +32,20 @@
     ;; amount_bytes -> edi      ; the amount of bytes that we want
     ;; alloc: return the amount the address of the page of the memory that we want
 alloc:
+    push rbp
+    mov rbp, rsp
+    cmp edi, CAPAICTY
+    ja error_alloc
+    jmp last_alloc
     
+    
+error_alloc:
+    mov rax, NULL                 ; putting a null address
 
+last_alloc:
+    pop rbp
+    ret
+    
 
     ;; void free(void *addr)
     ;; addr -> rdi              ; the address of the page of memory to free
--- projects/allocator/main.asm
+++ projects/allocator/main.asm
@@ -11,13 +11,35 @@
     EXIT_SUCCESS equ 0
     EXIT_FAILURE equ 1
 
+    SYS_write equ 1
+    STDOUT equ 1
+
+    NULL equ 0x0
+    LF equ 0xa
+    
+    ;; my variables
+    alloc_error_msg db "Error: trying to allocate memory", LF, NULL
+    alloc_error_msg_len equ $ - alloc_error_msg
+
 
     section .bss
 
     section .text
     global _start
 _start:
-    
+    mov rdi, 650000             ; put a lot of memory
+    call alloc
+
+    cmp rax, NULL               ; rax == null ; jump error
+    je error                    ; if there is an error print this 
+    jmp last                    ; otherwire we jump to finish the program
+
+error:
+    mov rax, SYS_write
+    mov rdi, STDOUT
+    mov rsi, alloc_error_msg
+    mov rdx, alloc_error_msg_len
+    syscall
     
 last:                           ; get out 
     mov rax, SYS_exit
